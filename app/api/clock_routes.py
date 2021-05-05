5c4e23eda0bd38acd3aa28a188bddee935de7d80
cubOlson <cub.olson@gmail.com>
2021-05-05 11:43:38 -0400

    initial

--- app/api/clock_routes.py
+++ app/api/clock_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from app.models import Clock
 
-user_routes = Blueprint('clocks', __name__)
+clock_routes = Blueprint('clock', __name__)
 
-@clocks_routes.route('/<int:id>')
+@clock_routes.route('/<int:id>')
 def getUserClock(id):
     clock = Clock.query.filter(Clock.user_id == id)
     return clock.to_dict()
